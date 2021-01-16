699a2e522b7d559f8cb98fd90ec350f1827c2b8d
SvetoslavPorozhanov <svetoslav_porozhanov@abv.bg>
2021-01-16 16:07:29 +0200

    Add login, logout

--- ng-petclinic/src/app/app.module.ts
+++ ng-petclinic/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PatientsListComponent } from './patients-list/patients-list.component';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PatientsListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ng-petclinic/src/app/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+
+@Injectable()
+
+export class UserService {
+
+  isLogged = false;
+  
+  constructor() { }
+
+  login(): void {
+    this.isLogged = true;
+  }
+
+  logout(): void {
+    this.isLogged = false;
+  }
+}
